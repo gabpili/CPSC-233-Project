17549f3bfab9cd64de31934fc7ca8758357de5e8
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2019-02-20 11:48:33 -0700

    Add files via upload
    first version of the static object class

---
+++ StaticObject.java
@@ -0,0 +1,37 @@
+public class StaticObject{
+  private double x;
+  private double y;
+  private String name;
+
+  public StaticObject(double x, double y, String name){
+    this.x = x;
+    this.y = y;
+    this.name = name;
+
+
+  }
+
+  public void setX(double x){
+    this.x = x;
+
+  }
+
+  public void setY(double y){
+    this.y = y;
+
+  }
+
+  public double getX(){
+    return x;
+
+  }
+
+  public double getY(){
+    return y;
+
+  }
+
+  public String toString(){
+    return name + "" + x + "" + y
+  }
+}
