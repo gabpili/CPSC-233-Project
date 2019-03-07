23a28cd911be76c652ef04ed85a46b50fb90efa3
shafinkheraj <47364518+shafinkheraj@users.noreply.github.com>
2019-03-06 17:48:35 -0700

    Add files via upload

--- Car.java
+++ Car.java
@@ -5,8 +5,8 @@ public class Car extends DynamicObject{
 	private double baseFriction;
 	private double angularVelocity = 0;
 
-	public Car(double x, double y, String name, double direction, double halfW, double halfH, double baseAcceleration, double baseFriction){
-		super(x,y,name,0,direction, halfW, halfH);
+	public Car(double x, double y, String name, double halfW, double halfH, double direction, double baseAcceleration, double baseFriction){
+		super(x, y, name, halfW, halfH, 0, direction);
 		this.baseAcceleration = baseAcceleration;
 		this.baseFriction = baseFriction;
 	}
@@ -35,7 +35,7 @@ public class Car extends DynamicObject{
 	}
 
 	public void tickExtended(double time){
-		super.setSpeed(super.getSpeed() - super.getSpeed() * baseFriction * time);
+		super.setSpeed(super.getSpeed() - super.getSpeed() * 0.2 * time);
 		super.setDirection(super.getDirection() + angularVelocity * time);
 	}
 
