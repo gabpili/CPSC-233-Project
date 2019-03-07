60a8529d38e8c0ea3509f56a5f40093208983101
shafinkheraj <47364518+shafinkheraj@users.noreply.github.com>
2019-03-06 17:48:25 -0700

    Add files via upload

--- StaticObstacle.java
+++ StaticObstacle.java
@@ -1,5 +1,9 @@
 public class StaticObstacle extends StaticObject{
-	public StaticObstacle(double x, double y, String name){
+	public StaticObstacle(double x, double y, String name, double halfW, double halfH){
 		super(x, y, name);
 	}
+
+@Override
+			public void resolveCollision(DynamicObject dObj){
+			}
 }
