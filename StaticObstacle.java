998abd70a62ac9b01d04ba1e151cb5bedbe3ee49
gabpili <47364598+gabpili@users.noreply.github.com>
2019-03-11 22:00:23 -0600

    someone document the override method please

--- StaticObstacle.java
+++ StaticObstacle.java
@@ -1,9 +1,17 @@
 public class StaticObstacle extends StaticObject{
+	/**
+	 * Constructor takes in five arguments: x, y, name, half width and half height
+	 * sets them using a constructor from the StaticObject class.
+	 */
 	public StaticObstacle(double x, double y, String name, double halfW, double halfH){
 		super(x, y, name, halfW, halfH);
 	}
-
-@Override
+			/**
+			 * Overriding method from StaticObject class that uses a constructor in the StaticObject
+			 * 
+			 * Prints out the list of flags
+	 		 */
+			@Override
 			public void resolveCollision(DynamicObject dObj){
 				super.addFlag(new Flag(Flag.HandlingMethod.DESTROY));
 				System.out.println(super.getFlags().get(0).getHandlingMethod());
