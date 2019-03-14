891d52ae90c2f14bf53c366495249efe3bb9d760
shafinkheraj <47364518+shafinkheraj@users.noreply.github.com>
2019-03-14 11:44:32 -0600

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,13 +1,16 @@
 import java.lang.Math;
 
 public class Wall extends BasicGameObject{
-	/* Instance Variables */
+	/**
+	* Instance Variables 
+	*/
 	private double x2;
 	private double y2;
 
 	/* Constructor
 	* Constructor consists arguments: x, y, name, x2 and y2
 	* x, y and name variables are extended from the BasicGameObject class using 'super' keyword
+	* takes the square root of  the exponent power of (x2 - x) and adds it to exponent power of (y2-y) and divides the result by 2
 	*/ 
 	public Wall(double x, double y, String name, double x2, double y2){
 		super(x, y, name, 0, Math.sqrt(Math.pow(x2 - x, 2) + Math.pow(y2 - y, 2)) / 2);
@@ -40,10 +43,7 @@ public class Wall extends BasicGameObject{
 	public double getY2(){
 		return y2;
 	}
- 
-    /* the method resolveCollision is overided from BasicGameObject
-    * argument name is dObj of type DynamicGameObject
-    */
+
     @Override
     public void resolveCollision(DynamicGameObject dObj){
 	}
