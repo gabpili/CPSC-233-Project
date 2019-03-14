a744f07ad321fde5607f992eb2c2c4a56f7eb331
shafinkheraj <47364518+shafinkheraj@users.noreply.github.com>
2019-03-14 11:20:25 -0600

    Update Wall.java

--- Wall.java
+++ Wall.java
@@ -1,38 +1,50 @@
 import java.lang.Math;
 
-public class Wall extends StaticObject{
+public class Wall extends BasicGameObject{
+	/* Instance Variables */
 	private double x2;
 	private double y2;
 
+	/* Constructor
+	* Constructor consists arguments: x, y, name, x2 and y2
+	* x, y and name variables are extended from the BasicGameObject class using 'super' keyword
+	*/ 
 	public Wall(double x, double y, String name, double x2, double y2){
 		super(x, y, name, 0, Math.sqrt(Math.pow(x2 - x, 2) + Math.pow(y2 - y, 2)) / 2);
 		setX2(x2);
 		setY2(y2);
 	}
-
+        /* Copy Constructor */
 	public Wall(Wall copy){
 		super(copy);
 		double x2 = copy.x2;
 		double y2 = copy.y2;
 	}
 
-		/* setters */
+       /* setter methods
+       * sets x2 and y2 
+       */
 	public void setX2(double x2){
 		this.x2 = x2;
 	}
-    public void setY2(double y2){
+        public void setY2(double y2){
 		this.y2 = y2;
-    }
-
-	 /*getters */
+        }
+  
+    /*getter methods 
+    * Returns x2 and y2
+    */
     public double getX2(){
         return x2;
     }
 	public double getY2(){
 		return y2;
 	}
-
-	@Override
-	public void resolveCollision(DynamicObject dObj){
+ 
+    /* the method resolveCollision is overided from BasicGameObject
+    * argument name is dObj of type DynamicGameObject
+    */
+    @Override
+    public void resolveCollision(DynamicGameObject dObj){
 	}
 }
