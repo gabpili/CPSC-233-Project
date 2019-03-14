e80cedd2e00ea49456270354641ff9960318c2fa
shafinkheraj <47364518+shafinkheraj@users.noreply.github.com>
2019-03-14 11:34:01 -0600

    Update FinishLine.java

--- FinishLine.java
+++ FinishLine.java
@@ -2,17 +2,27 @@ public class FinishLine extends Checkpoint{
 
     private int lastNumber;
 
+	 /**
+	 * Constructor takes in six arguments: x, y, name, x2, y2, lastNumber
+	 *  x, y, name, x2 and y2 variables are extended from the CheckPoint class using 'super' keyword
+	 */
 	public FinishLine (double x, double y, String name, double x2, double y2, int lastNumber) {
 		super(x, y, name, x2, y2, 0);
 		this.lastNumber = lastNumber;
 
 	}
-
+        /**
+	*Copy Constructor
+	*/
 	public FinishLine(FinishLine copy) {
 		super(copy);
 		copy.lastNumber = lastNumber;
 
 	}
+
+    /**
+    * adds a next lap flag to the car 
+    */
 
 	@Override
 	public void resolveCollision(DynamicObject dObj) {
