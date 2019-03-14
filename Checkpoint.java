5e1b0ccd4caca1bdd1a1e6d464239106426e718e
gabpili <47364598+gabpili@users.noreply.github.com>
2019-03-14 11:19:09 -0600

    Update Checkpoint.java
    line 42

--- Checkpoint.java
+++ Checkpoint.java
@@ -1,30 +1,48 @@
 public class Checkpoint extends Wall{
+	
+	/**
+	 * Instance variables
+	 */
 	private int number;
 
+	/**
+	 * Constructor that takes in 6 arguments and sets given the startX, startY, name 
+	 * endX and endY values using a constructor in the Wall class and sets number given
+	 * number value using method within this class.
+	 */
 	public Checkpoint (double x, double y, String name, double x2, double y2, int number) {
 		super(x, y, name, x2, y2);
 		setNumber(number);
 
 	}
-
+	
+	/**
+	 * Copy constructor.
+	 */
 	public Checkpoint(Checkpoint copy) {
 		super(copy);
 		this.number = copy.number;
 	}
 
-	/* setters */
+	/**
+	 * Sets given number with number variable.
+	 */
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
-	/*getters */
+	/**
+	 * Returns the value of the number.
+	 */
 	 public int getNumber() {
 		return number;
 	}
 
-
+	/**
+	 * Override method that takes in a DynamicGameObject and adds a 
+	 */
 	@Override
-	public void resolveCollision(DynamicObject dObj) {
+	public void resolveCollision(DynamicGameObject dObj) {
 		dObj.addFlag(new Flag(number, Flag.HandlingMethod.NEXT_SECTION));
 
 	}
