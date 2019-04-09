37e7aed0e417e32bef9d630be0e27a7470512961
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2019-04-09 16:47:12 -0600

    comment

--- gameobj/SpeedboostPickup.java
+++ gameobj/SpeedboostPickup.java
@@ -3,24 +3,37 @@ package gameobj;
 import base.Flag;
 
 public class SpeedboostPickup extends Pickup {
+
     public static final double COOLDOWN = 5.0;
     public static final double SPEED = 20.0;
 
+    /**
+	 * basic constructor with name initialized as "Speedboost Pickup"
+	 */
     public SpeedboostPickup(double x, double y) {
         super(x, y, "Speedboost Pickup");
 
     }
 
+    /**
+	 * default constructor with x, y initialized as 0s
+	 */
     public SpeedboostPickup() {
         this(0, 0);
 
     }
 
+    /**
+	 * copy constructor
+	 */
     public SpeedboostPickup(SpeedboostPickup toCopy) {
         super(toCopy);
 
     }
 
+    /**
+	 * resolves collision by disabling the pickup and the colliding car picking up the speed boost
+	 */
     @Override
     public void resolveCollision(DynamicGameObject dObj) throws IllegalArgumentException {
     	if (getActive()) {
