fe3f2e27786abd5aa2c6953ff39ac5056bbf8dbd
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2019-04-09 16:54:22 -0600

    commetns

--- gameobj/Wall.java
+++ gameobj/Wall.java
@@ -5,19 +5,17 @@ import java.lang.Math;
 import base.Flag;
 
 public class Wall extends BasicGameObject{
-	/**
-	 * Instance Variables 
-	 */
+
 	private double startX;
 	private double startY;
 	private double endX;
 	private double endY;
-	
+
 	/**
 	 * Constructor consists arguments: x, y, name, x2 and y2
-	 * values are set using a constructor in the BasicGameObject 
+	 * values are set using a constructor in the BasicGameObject
 	 * and setter methods within this Wall class.
-	 */ 
+	 */
 	public Wall(double startX, double startY, String name, double endX, double endY) {
 		super(startX, startY, name, 0, Math.sqrt(Math.pow(endX - startX, 2) + Math.pow(endY - startY, 2)) / 2, 0);
 		setStartX(startX);
@@ -25,9 +23,9 @@ public class Wall extends BasicGameObject{
 		setEndX(endX);
 		setEndY(endY);
 	}
-	
+
     /**
-	 * Copy Constructor 
+	 * Copy Constructor
 	 */
 	public Wall(Wall copy) {
 
@@ -37,56 +35,56 @@ public class Wall extends BasicGameObject{
 		endX = copy.endX;
 		endY = copy.endY;
 	}
-	
-    /** 
+
+    /**
      * Sets given value of startX to this.startX
      */
 	public void setStartX(double startX) {
 		this.startX = startX;
 	}
-	
-    /** 
+
+    /**
      * Sets given value of startY to this.startY
      */
 	public void setStartY(double startY) {
 		this.startY = startY;
 	}
-	
-    /** 
+
+    /**
      * Sets methods sets given value of endX to this.endX
      */
 	public void setEndX(double endX) {
 		this.endX = endX;
 	}
-	
-    /** 
+
+    /**
      * Sets methods sets given value of endY to this.endY
      */
     public void setEndY(double endY) {
-	this.endY = endY;
+		this.endY = endY;
     }
-	
+
     /**
      * Returns value of startX
      */
     public double getStartX() {
         return startX;
     }
-	
+
     /**
      * Returns value of startY
      */
 	public double getStartY() {
 		return startY;
 	}
-    
+
     /**
      * Getter methods returns value of endX
      */
     public double getEndX() {
         return endX;
     }
-	
+
     /**
      * Getter methods returns value of endY
      */
@@ -104,7 +102,7 @@ public class Wall extends BasicGameObject{
 		return Math.sqrt((dx * dx) + (dy * dy));
 
 	}
-	
+
     /**
 	 * The method resolveCollision is overidden from BasicGameObject
      * argument name is dObj of type DynamicGameObject
