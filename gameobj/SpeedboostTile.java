cab5cee0fb7a10e626ec354ebe983c2d0039b7d0
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2019-04-10 17:16:50 -0600

    Update SpeedboostTile.java

--- gameobj/SpeedboostTile.java
+++ gameobj/SpeedboostTile.java
@@ -5,22 +5,22 @@ import base.Flag;
 public class SpeedboostTile extends DynamicGameObject{
 
     /**
-    * full constructor with halfW initialized to 1.5m and halfH initialized to 2m
-    */
+     * full constructor with halfW initialized to 1.5m and halfH initialized to 2m
+     */
     public SpeedboostTile(double x, double y, double direction){
         super(x, y, "Speedboost", 1.5, 2, 0, 0, direction);
     }
 
     /**
-    * copy constructor
-    */
+     * copy constructor
+     */
     public SpeedboostTile(SpeedboostTile toCopy){
         super(toCopy);
     }
 
     /**
-    * resolve collison by adding speed to the car 
-    */
+     * resolve collison by adding speed to the car 
+     */
     @Override
     public void resolveCollision(DynamicGameObject dObj) throws IllegalArgumentException {
         dObj.addFlag(new Flag(Flag.HandlingMethod.ADD_SPEED, 2));
