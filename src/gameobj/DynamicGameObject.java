b79d393fd0ca332c7773a2262593611c62839932
Nekonome9000 <47364593+Nekonome9000@users.noreply.github.com>
2019-04-12 15:32:25 -0600

    code revised and comments added and or fixed

--- src/gameobj/DynamicGameObject.java
+++ src/gameobj/DynamicGameObject.java
@@ -24,13 +24,13 @@ public abstract class DynamicGameObject extends BasicGameObject {
         this(x, y, 0, name, halfW, halfH, mass, 0);
 
     }
-    
+
 	/**
-	 * Constructor takes in seven arguments and uses methods within the class to set speed with given 
-     * speed and direction with given direction as well as a constructor from the StaticObject class 
-     * to set x, y, name, halfW and halfH. 
+	 * Constructor takes in seven arguments and uses methods within the class to set speed with given
+     * speed and direction with given direction as well as a constructor from the StaticObject class
+     * to set x, y, name, halfW and halfH.
 	 */
-    public DynamicGameObject(double x, double y, double direction, String name, double halfW, double halfH, double mass, 
+    public DynamicGameObject(double x, double y, double direction, String name, double halfW, double halfH, double mass,
     	double speed) {
         super(x, y, direction, name, halfW, halfH, mass);
         setSpeed(speed);
@@ -38,16 +38,19 @@ public abstract class DynamicGameObject extends BasicGameObject {
 
     }
 
-    public DynamicGameObject(double x, double y, Vector direction, String name, double halfW, double halfH, double mass, 
+    /**
+     * Constructor takes seven arguments and uses type vector when given a direction and velocity
+     */
+    public DynamicGameObject(double x, double y, Vector direction, String name, double halfW, double halfH, double mass,
     	Vector velocity) {
     	super(x, y, direction, name, halfW, halfH, mass);
     	setVelocity(velocity);
         this.inertiaMoment = mass * (Math.pow(halfW * 2, 2) + Math.pow(halfH * 2, 2)) / 12;
 
     }
-    
+
 	/**
-     * copy constructor 
+     * copy constructor
 	 */
     public DynamicGameObject(DynamicGameObject toCopy) {
         super(toCopy);
@@ -56,66 +59,102 @@ public abstract class DynamicGameObject extends BasicGameObject {
 
     }
 
+    /**
+     * Setter method sets given 'speed' to variable 'velocity'
+     */
     public void setSpeed(double speed) {
     	if (velocity.normSqr() == 0) {
     		velocity = getDirection().multiply(speed);
-    		
+
     	}else {
     		velocity = velocity.normalize().multiply(speed);
 
     	}
     }
 
+    /**
+     * Setter method sets given 'speed' and give 'direction' to the variable 'velocity'
+     */
     public void setVelocity(double speed, double direction) {
     	this.velocity = new Vector(direction).multiply(speed);
 
     }
 
+    /**
+     * Setter method sets given 'velocity' to the variable 'velocity'
+     */
     public void setVelocity(Vector velocity) {
     	this.velocity = new Vector(velocity);
 
     }
 
+    /**
+     * Setter method sets given 'angularVelocity' to the variable 'angularVelocity'
+     */
     public void setAngularVelocity(double angularVelocity) {
         this.angularVelocity = angularVelocity;
 
     }
 
+    /**
+     * Setter method sets given 'netForce' to the variable 'netForce'
+     */
     public void setNetForce(Vector netForce) {
         this.netForce = netForce;
 
     }
 
+    /**
+     * Setter method sets given 'netAngularAcceleration' to the variable 'netAngularAcceleration'
+     */
     public void setNetAngularAcceleration(double netAngularAcceleration) {
         this.netAngularAcceleration = netAngularAcceleration;
 
     }
 
+    /**
+     * Getter method returns the value of velocity normal
+     */
     public double getSpeed() {
     	return velocity.norm();
 
     }
 
+    /**
+     * Getter method returns the value of velocity
+     */
     public Vector getVelocity() {
     	return new Vector(velocity);
 
     }
 
+    /**
+     * Getter method returns the value of angularVelocity
+     */
     public double getAngularVelocity() {
         return angularVelocity;
 
     }
 
+    /**
+     * Getter method returns the value of netForce
+     */
     public Vector getNetForce() {
         return netForce;
 
     }
 
+    /**
+     * Getter method returns the value of netAngularAcceleration
+     */
     public double getNetAngularAcceleration() {
         return netAngularAcceleration;
 
     }
 
+    /**
+     * Getter method returns the value of netAngularAcceleration
+     */
     public double getInertiaMoment() {
         return inertiaMoment;
 
@@ -137,6 +176,9 @@ public abstract class DynamicGameObject extends BasicGameObject {
 
     }
 
+    /**
+     * adds given angularAcceleration to the netAngularAcceleration
+     */
     public void addAngularAcceleration(double angularAcceleration) {
         netAngularAcceleration += angularAcceleration;
 
@@ -165,11 +207,11 @@ public abstract class DynamicGameObject extends BasicGameObject {
 
 	/**
      * returns values into string format using the toString() method in StaticObject
-     * and concatinating it with what was calculated for in this class such as speed and the 
-     * degree of the direction. 
+     * and concatinating it with what was calculated for in this class such as speed and the
+     * degree of the direction.
 	 */
     public String toString() {
         return super.toString() + (int) velocity.norm() +  "m/s ";
-        
+
     }
 }
