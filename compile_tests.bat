0ed850ff9052541b71b97a5bdfb53e13ae363060
Sean <44074685+WelcomeBakC@users.noreply.github.com>
2019-04-13 00:16:51 -0600

    for testing

---
+++ compile_tests.bat
@@ -0,0 +1,6 @@
+
+javac -sourcepath src -cp .;ju.jar;ham.jar -d bin src\gameobj\BasicGameObject.java
+javac -sourcepath src -cp .;ju.jar;ham.jar -d bin src\BasicGameObjectTest.java
+javac -sourcepath src -cp .;ju.jar;ham.jar -d bin src\gameobj\DynamicGameObject.java
+javac -sourcepath src -cp .;ju.jar;ham.jar -d bin src\DynamicGameObjectTest.java
+pause
---
+++ run_tests.bat
@@ -0,0 +1,4 @@
+cd bin
+java -cp .;ju.jar;ham.jar org.junit.runner.JUnitCore BasicGameObjectTest
+java -cp .;ju.jar;ham.jar org.junit.runner.JUnitCore DynamicGameObjectTest
+pause
